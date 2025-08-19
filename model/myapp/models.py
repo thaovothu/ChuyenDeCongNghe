23b365a70a159619cd08f40fa9748c6e39a1ddc9
vothithao <0945206559vtt@gmail.com>
2025-08-19 22:09:05 +0700

    Add initial models and migrations_7

--- model/myapp/models.py
+++ model/myapp/models.py
@@ -2,30 +2,34 @@ from django.db import models
 from datetime import date
 
 
-class Person(models.Model):
-    name = models.CharField(max_length=128)
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
 
     def __str__(self):
         return self.name
 
 
-class Group(models.Model):
-    name = models.CharField(max_length=128)
-    members = models.ManyToManyField(Person, through="Membership")
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
 
     def __str__(self):
         return self.name
 
 
-class Membership(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    date_joined = models.DateField()
-    invite_reason = models.CharField(max_length=64)
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField(default=date.today)
+    authors = models.ManyToManyField(Author)
+    number_of_comments = models.IntegerField(default=0)
+    number_of_pingbacks = models.IntegerField(default=0)
+    rating = models.IntegerField(default=5)
 
-class Meta:
-    constraints = [
-        models.UniqueConstraint(
-            fields=["person", "group"], name="unique_person_group"
-        )
-    ]
+    def __str__(self):
+        return self.headline
