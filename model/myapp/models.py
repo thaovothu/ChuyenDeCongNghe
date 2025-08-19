f6235d87499eacd4d4b3c7c01af2d68ea89e47d4
vothithao <0945206559vtt@gmail.com>
2025-08-19 21:27:29 +0700

    Add initial models and migrations_3

--- model/myapp/models.py
+++ model/myapp/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
-class Musician(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    instrument = models.CharField(max_length=100)
-
-
-class Album(models.Model):
-    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_stars = models.IntegerField()
+class Runner(models.Model):
+    MedalType = models.TextChoices("MedalType", "GOLD SILVER BRONZE")
+    name = models.CharField(max_length=60)
+    medal = models.CharField(blank=True, choices=MedalType, max_length=10)
